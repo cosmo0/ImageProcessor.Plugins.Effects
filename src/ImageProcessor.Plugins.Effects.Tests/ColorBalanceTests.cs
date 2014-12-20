cbe7e7d0073f25ad0b4f53a4f7cef2ec7d14564d
Thomas Broust <thomas.broust@gmail.com>
2014-12-20 19:49:31 +0100

    Adds new (failing, not even compiling) test

--- src/ImageProcessor.Plugins.Effects.Tests/ColorBalanceTests.cs
+++ src/ImageProcessor.Plugins.Effects.Tests/ColorBalanceTests.cs
@@ -1,13 +1,36 @@
 ﻿namespace ImageProcessor.Plugins.Effects.Tests
 {
+    using System;
+    using System.IO;
     using Xunit;
+    using FluentAssertions;
 
     public class ColorBalanceTests
     {
         [Fact]
-        public void ColorBalance_loads_image()
+        public void ColorBalance_processes_image()
         {
+            foreach (var file in Directory.GetFiles(@".\img"))
+            {
+                // arrange
+                using (var factory = new ImageFactory())
+                {
+                    factory.Load(file);
 
+                    ColorBalance processor = new ColorBalance();
+                    processor.DynamicParameter = new ColorBalanceParameters();
+
+                    // act
+                    Action act = () =>
+                        {
+                            Image img = processor.ProcessImage(factory);
+                            img.Save(string.Format(@".\img\{0}_result.jpg", Path.GetFileNameWithoutExtension(file)));
+                        };
+
+                    // assert
+                    act.ShouldNotThrow("because the image should have been processed without error");
+                }
+            }
         }
     }
 }
---
+++ src/ImageProcessor.Plugins.Effects/ColorBalance.cs
@@ -0,0 +1,21 @@
+﻿namespace ImageProcessor.Plugins.Effects
+{
+    using System.Collections.Generic;
+    using System.Drawing;
+    using ImageProcessor.Processors;
+
+    /// <summary>
+    /// Provides color balance modifications
+    /// </summary>
+    public class ColorBalance : IGraphicsProcessor
+    {
+        public Image ProcessImage(ImageFactory factory)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public dynamic DynamicParameter { get; set; }
+
+        public Dictionary<string, string> Settings { get; set; }
+    }
+}
