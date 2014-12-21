d3fc92a74c14cdc687edd5fe7288b359e2e5325b
Thomas Broust <thomas.broust@gmail.com>
2014-12-21 18:52:35 +0100

    Adds unit test to compare the results of two different parameters

--- src/ImageProcessor.Plugins.Effects.Tests/ColorBalanceTests.cs
+++ src/ImageProcessor.Plugins.Effects.Tests/ColorBalanceTests.cs
@@ -8,12 +8,18 @@
 
     public class ColorBalanceTests
     {
+        private readonly string[] images;
+
+        public ColorBalanceTests()
+        {
+            string path = Path.Combine(Path.GetDirectoryName(typeof(ColorBalanceTests).Assembly.Location), "img");
+            images = new string[] { Path.Combine(path, "1.jpg") };
+        }
+
         [Fact]
         public void ColorBalance_processes_image()
         {
-            string imgs = Path.Combine(Path.GetDirectoryName(typeof(ColorBalanceTests).Assembly.Location), "img");
-
-            foreach (string file in Directory.GetFiles(imgs))
+            foreach (string file in images)
             {
                 // arrange
                 using (ImageFactory factory = new ImageFactory())
@@ -25,13 +31,40 @@
 
                     // act
                     Action act = () =>
-                        {
-                            Image img = processor.ProcessImage(factory);
-                            img.Save(string.Format("{0}/{1}_result.jpg", imgs, Path.GetFileNameWithoutExtension(file)));
-                        };
+                    {
+                        Image img = processor.ProcessImage(factory);
+                        img.Save(string.Format("{0}/{1}_result.jpg", Path.GetDirectoryName(file), Path.GetFileNameWithoutExtension(file)));
+                    };
 
                     // assert
                     act.ShouldNotThrow("because the image should have been processed without error");
+                }
+            }
+        }
+
+        [Fact]
+        public void Different_values_yield_different_images()
+        {
+            foreach (string file in images)
+            {
+                // arrange
+                using (ImageFactory factory = new ImageFactory())
+                {
+                    factory.Load(file);
+
+                    ColorBalance processor = new ColorBalance();
+                    processor.DynamicParameter = new ColorBalanceParameters();
+
+                    ColorBalance processor2 = new ColorBalance();
+                    processor2.DynamicParameter = new ColorBalanceParameters() { Blue = 155, Green = 155, Red = 155 };
+
+                    // act
+                    Bitmap result = new Bitmap(processor.ProcessImage(factory));
+                    Bitmap result2 = new Bitmap(processor2.ProcessImage(factory));
+                    result2.Save(string.Format("{0}/{1}_result2.jpg", Path.GetDirectoryName(file), Path.GetFileNameWithoutExtension(file)));
+
+                    // assert
+                    result.Equals(result2).Should().BeFalse("because different parameters should yield different images");
                 }
             }
         }
---
+++ src/ImageProcessor.Plugins.Effects.Tests/ImageComparer.cs
@@ -0,0 +1,54 @@
+﻿namespace ImageProcessor.Plugins.Effects.Tests
+{
+    using System.Drawing;
+    using System.Drawing.Imaging;
+
+    public static class ImageComparer
+    {
+        /// <summary>
+        /// Compares two images
+        /// </summary>
+        /// <remarks>
+        /// See: http://codereview.stackexchange.com/a/39987/43027
+        /// </remarks>
+        /// <param name="bmp1">The first bitmap</param>
+        /// <param name="bmp2">The second bitmap</param>
+        public static bool Equals(this Bitmap bmp1, Bitmap bmp2)
+        {
+            if (bmp1.Width != bmp2.Width || bmp1.Height != bmp1.Height)
+            {
+                return false;
+            }
+
+            bool equals = true;
+            Rectangle rect = new Rectangle(0, 0, bmp1.Width, bmp1.Height);
+            BitmapData bmpData1 = bmp1.LockBits(rect, ImageLockMode.ReadOnly, bmp1.PixelFormat);
+            BitmapData bmpData2 = bmp2.LockBits(rect, ImageLockMode.ReadOnly, bmp2.PixelFormat);
+
+            unsafe {
+                byte* ptr1 = (byte*)bmpData1.Scan0.ToPointer();
+                byte* ptr2 = (byte*)bmpData2.Scan0.ToPointer();
+                int width = rect.Width * 3; // for 24bpp pixel data
+                for (int y = 0; equals && y < rect.Height; y++) {
+                    for (int x = 0; x < width; x++) {
+                        if (*ptr1 != *ptr2) {
+                            equals = false;
+                            break;
+                        }
+
+                        ptr1++;
+                        ptr2++;
+                    }
+
+                    ptr1 += bmpData1.Stride - width;
+                    ptr2 += bmpData2.Stride - width;
+                }
+            }
+
+            bmp1.UnlockBits(bmpData1);
+            bmp2.UnlockBits(bmpData2);
+
+            return equals;
+        }
+    }
+}
