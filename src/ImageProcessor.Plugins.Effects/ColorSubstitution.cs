71774170061043e8ebf3bf3e58aed84780e20fda
Thomas Broust <thomas.broust@gmail.com>
2014-12-29 21:35:06 +0100

    Implements ProcessorBase in color substitution

--- src/ImageProcessor.Plugins.Effects/ColorSubstitution.cs
+++ src/ImageProcessor.Plugins.Effects/ColorSubstitution.cs
@@ -13,123 +13,85 @@
     /// Substitutes a color for another
     /// http://softwarebydefault.com/2013/03/16/bitmap-color-substitution/
     /// </summary>
-    public class ColorSubstitution : IGraphicsProcessor
+    public class ColorSubstitution : ProcessorBase
     {
-        public dynamic DynamicParameter { get; set; }
-
-        public Dictionary<string, string> Settings { get; set; }
-
-        public Image ProcessImage(ImageFactory factory)
+        protected override byte[] Process(byte[] pixelBuffer, int sourceWidth, int sourceHeight, int sourceStride)
         {
-            Bitmap sourceBitmap = (new Bitmap(factory.Image)).Format32bppArgbCopy();
             ColorSubstitutionParameters parameters = this.DynamicParameter;
 
-            try
+            byte sourceRed = 0, sourceGreen = 0, sourceBlue = 0, sourceAlpha = 0;
+            int resultRed = 0, resultGreen = 0, resultBlue = 0;
+
+            byte newRedValue = parameters.ReplaceWith.R;
+            byte newGreenValue = parameters.ReplaceWith.G;
+            byte newBlueValue = parameters.ReplaceWith.B;
+
+            byte redFilter = parameters.ToReplace.R;
+            byte greenFilter = parameters.ToReplace.G;
+            byte blueFilter = parameters.ToReplace.B;
+
+            byte minValue = 0;
+            byte maxValue = 255;
+
+            for (int k = 0; k < pixelBuffer.Length; k += 4)
             {
-                Bitmap resultBitmap = new Bitmap(sourceBitmap.Width, sourceBitmap.Height, PixelFormat.Format32bppArgb);
+                sourceAlpha = pixelBuffer[k + 3];
 
-                BitmapData sourceData = sourceBitmap.LockBits(
-                    new Rectangle(0, 0, sourceBitmap.Width, sourceBitmap.Height),
-                    ImageLockMode.ReadOnly,
-                    PixelFormat.Format32bppArgb);
-                BitmapData resultData = resultBitmap.LockBits(
-                    new Rectangle(0, 0, resultBitmap.Width, resultBitmap.Height),
-                    ImageLockMode.WriteOnly,
-                    PixelFormat.Format32bppArgb);
-
-                byte[] resultBuffer = new byte[resultData.Stride * resultData.Height];
-                Marshal.Copy(sourceData.Scan0, resultBuffer, 0, resultBuffer.Length);
-
-                sourceBitmap.UnlockBits(sourceData);
-
-                byte sourceRed = 0, sourceGreen = 0, sourceBlue = 0, sourceAlpha = 0;
-                int resultRed = 0, resultGreen = 0, resultBlue = 0;
-
-                byte newRedValue = parameters.ReplaceWith.R;
-                byte newGreenValue = parameters.ReplaceWith.G;
-                byte newBlueValue = parameters.ReplaceWith.B;
-
-                byte redFilter = parameters.ToReplace.R;
-                byte greenFilter = parameters.ToReplace.G;
-                byte blueFilter = parameters.ToReplace.B;
-
-                byte minValue = 0;
-                byte maxValue = 255;
-
-                for (int k = 0; k < resultBuffer.Length; k += 4)
+                if (sourceAlpha != 0)
                 {
-                    sourceAlpha = resultBuffer[k + 3];
+                    sourceBlue = pixelBuffer[k];
+                    sourceGreen = pixelBuffer[k + 1];
+                    sourceRed = pixelBuffer[k + 2];
 
-                    if (sourceAlpha != 0)
+                    if ((sourceBlue < blueFilter + parameters.Threshold &&
+                        sourceBlue > blueFilter - parameters.Threshold) &&
+                        (sourceGreen < greenFilter + parameters.Threshold &&
+                            sourceGreen > greenFilter - parameters.Threshold) &&
+                        (sourceRed < redFilter + parameters.Threshold &&
+                            sourceRed > redFilter - parameters.Threshold))
                     {
-                        sourceBlue = resultBuffer[k];
-                        sourceGreen = resultBuffer[k + 1];
-                        sourceRed = resultBuffer[k + 2];
+                        resultBlue = blueFilter - sourceBlue + newBlueValue;
 
-                        if ((sourceBlue < blueFilter + parameters.Threshold &&
-                             sourceBlue > blueFilter - parameters.Threshold) &&
-                            (sourceGreen < greenFilter + parameters.Threshold &&
-                             sourceGreen > greenFilter - parameters.Threshold) &&
-                            (sourceRed < redFilter + parameters.Threshold &&
-                             sourceRed > redFilter - parameters.Threshold))
+                        if (resultBlue > maxValue)
                         {
-                            resultBlue = blueFilter - sourceBlue + newBlueValue;
-
-                            if (resultBlue > maxValue)
-                            {
-                                resultBlue = maxValue;
-                            }
-                            else if (resultBlue < minValue)
-                            {
-                                resultBlue = minValue;
-                            }
-
-                            resultGreen = greenFilter - sourceGreen + newGreenValue;
-
-                            if (resultGreen > maxValue)
-                            {
-                                resultGreen = maxValue;
-                            }
-                            else if (resultGreen < minValue)
-                            {
-                                resultGreen = minValue;
-                            }
-
-                            resultRed = redFilter - sourceRed + newRedValue;
-
-                            if (resultRed > maxValue)
-                            {
-                                resultRed = maxValue;
-                            }
-                            else if (resultRed < minValue)
-                            {
-                                resultRed = minValue;
-                            }
-
-                            resultBuffer[k] = (byte)resultBlue;
-                            resultBuffer[k + 1] = (byte)resultGreen;
-                            resultBuffer[k + 2] = (byte)resultRed;
-                            resultBuffer[k + 3] = sourceAlpha;
+                            resultBlue = maxValue;
                         }
+                        else if (resultBlue < minValue)
+                        {
+                            resultBlue = minValue;
+                        }
+
+                        resultGreen = greenFilter - sourceGreen + newGreenValue;
+
+                        if (resultGreen > maxValue)
+                        {
+                            resultGreen = maxValue;
+                        }
+                        else if (resultGreen < minValue)
+                        {
+                            resultGreen = minValue;
+                        }
+
+                        resultRed = redFilter - sourceRed + newRedValue;
+
+                        if (resultRed > maxValue)
+                        {
+                            resultRed = maxValue;
+                        }
+                        else if (resultRed < minValue)
+                        {
+                            resultRed = minValue;
+                        }
+
+                        pixelBuffer[k] = (byte)resultBlue;
+                        pixelBuffer[k + 1] = (byte)resultGreen;
+                        pixelBuffer[k + 2] = (byte)resultRed;
+                        pixelBuffer[k + 3] = sourceAlpha;
                     }
                 }
-
-                Marshal.Copy(resultBuffer, 0, resultData.Scan0, resultBuffer.Length);
-                resultBitmap.UnlockBits(resultData);
-
-                sourceBitmap = resultBitmap;
-            }
-            catch (Exception ex)
-            {
-                if (sourceBitmap != null)
-                {
-                    sourceBitmap.Dispose();
-                }
-
-                throw new ImageProcessingException("Error processing image with " + this.GetType().Name, ex);
             }
 
-            return sourceBitmap;
+            return pixelBuffer;
         }
     }
 }
