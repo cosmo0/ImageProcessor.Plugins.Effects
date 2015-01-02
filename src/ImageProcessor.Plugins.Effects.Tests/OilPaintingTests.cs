072502f95965d4a2ded337a2ff20231dd5cb1bde
Thomas Broust <thomas.broust@gmail.com>
2015-01-02 17:21:00 +0100

    Adds tests for the oil painting filter

--- src/ImageProcessor.Plugins.Effects.Tests/OilPaintingTests.cs
+++ src/ImageProcessor.Plugins.Effects.Tests/OilPaintingTests.cs
@@ -9,6 +9,60 @@
     public class OilPaintingTests : BaseTest
     {
         [Fact]
+        public void Different_filter_size_yield_different_images()
+        {
+            foreach (string file in this.images)
+            {
+                // arrange
+                using (ImageFactory factory = new ImageFactory())
+                {
+                    factory.Load(file);
+
+                    OilPainting processor = new OilPainting();
+                    processor.DynamicParameter = new OilPaintingParameters() { FilterSize = 10 };
+
+                    OilPainting processor2 = new OilPainting();
+                    processor2.DynamicParameter = new OilPaintingParameters() { FilterSize = 20 };
+
+                    // act
+                    Bitmap result = new Bitmap(processor.ProcessImage(factory));
+                    Bitmap result2 = new Bitmap(processor2.ProcessImage(factory));
+                    result2.Save(string.Format("{0}/{1}_oil_filtersize2.jpg", Path.GetDirectoryName(file), Path.GetFileNameWithoutExtension(file)));
+
+                    // assert
+                    result.Equals(result2).Should().BeFalse("because different parameters should yield different images");
+                }
+            }
+        }
+
+        [Fact]
+        public void Different_levels_yield_different_images()
+        {
+            foreach (string file in this.images)
+            {
+                // arrange
+                using (ImageFactory factory = new ImageFactory())
+                {
+                    factory.Load(file);
+
+                    OilPainting processor = new OilPainting();
+                    processor.DynamicParameter = new OilPaintingParameters() { Levels = 10 };
+
+                    OilPainting processor2 = new OilPainting();
+                    processor2.DynamicParameter = new OilPaintingParameters() { Levels = 20 };
+
+                    // act
+                    Bitmap result = new Bitmap(processor.ProcessImage(factory));
+                    Bitmap result2 = new Bitmap(processor2.ProcessImage(factory));
+                    result2.Save(string.Format("{0}/{1}_oil_levels2.jpg", Path.GetDirectoryName(file), Path.GetFileNameWithoutExtension(file)));
+
+                    // assert
+                    result.Equals(result2).Should().BeFalse("because different parameters should yield different images");
+                }
+            }
+        }
+
+        [Fact]
         public override void Image_is_processed()
         {
             foreach (string file in this.images)
@@ -25,7 +79,7 @@
                     Action act = () =>
                     {
                         Image img = processor.ProcessImage(factory);
-                        img.Save(string.Format("{0}/{1}_oilpainting_default.jpg", Path.GetDirectoryName(file), Path.GetFileNameWithoutExtension(file)));
+                        img.Save(string.Format("{0}/{1}_oil_default.jpg", Path.GetDirectoryName(file), Path.GetFileNameWithoutExtension(file)));
                     };
 
                     // assert
